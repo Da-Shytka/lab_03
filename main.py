1ad7d15510c82dce2d30cded27e075e32e69937c
Дашка <Romanova080903@yandex.ru>
2023-10-02 17:22:13 +0500

    Метод пузырька (по направлению)

--- main.py
+++ main.py
@@ -4,9 +4,21 @@ mas = []
 for i in range(n):
     mas.append(int(input("Введите число: ")))
 
-for i in range(n - 1):
-    for j in range(n - i - 1):
-        if mas[j] > mas[j + 1]:
-            mas[j], mas[j + 1] = mas[j + 1], mas[j]
+sort = int(input("Если хотите отсортировать по возрастанию, то введите 1, если по убыванию, то введите 2: "))
+
+if(sort == 1):
+    for i in range(n - 1):
+        for j in range(n - i - 1):
+            if mas[j] > mas[j + 1]:
+                mas[j], mas[j + 1] = mas[j + 1], mas[j]
+
+elif (sort == 2):
+    for i in range(n - 1):
+        for j in range(n - i - 1):
+            if mas[j] < mas[j + 1]:
+                mas[j], mas[j + 1] = mas[j + 1], mas[j]
+
+else:
+    print("Вы ввели не верную комбинацию")
 
 print(mas)
